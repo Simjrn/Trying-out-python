aa0b1da5788be7ad356bb9e4f10f47a88816482d
Simjrn <simonjenni374@gmail.com>
2025-07-31 10:42:36 +0100

    Update and rename Trying-out-python-code.py to Trying_out_python_code.py

---
+++ Trying_out_python_code.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import pandas as pd
+import time
+
+df = ""
+Q1 = ""
+
+
+st.sidebar.title("Which lesson")
+st.sidebar.write("Choose which lesson you're on")
+page = st.sidebar.radio(
+        "Go to",
+        ["Welcome", "Lesson 1", "Lesson 2", "Chat 1"]
+    )
+
+if page == "Lesson 1":
+    if st.button("Start"):
+        df = pd.DataFrame({
+        'English': ["Hello", "Good morning", "Good afternoon", "How"],
+        'Romansh': ["Allegra", "Bun di", "Buna saira", "Co"]
+        })
+
+        df
+        st.markdown("---")
+        st.markdown("---")
+        
+    
+    Q1 = st.text_area("What is 'Hello'?")
+    if Q1 == "Allegra":
+        Q2 = st.text_area("What is 'How'?")
+        if Q2 == "Co":
+            Q3 = st.text_area("What is 'Hello, how?'?")
+            if Q3 == "Allegra, co?":
+                Q4 = st.text_area("What is 'Good afternoon'?")
+                if Q4 == "Buna saira":
+                    Q5 = st.text_area("What is 'Good morning'?")
+                    if Q5 == "Bun di":
+                        Q6 = st.text_area("What is 'Hello, good morning'?")
+                        if Q6 == "Allegra, bun di":
+                            Q7 = st.text_area("What is 'Good afternoon, how?'?")
+                            if Q7 == "Buna saira, co?":
+                                Q8 = st.text_area("What is 'Good afternoon, good morning'?")
+                                if Q8 == "Buna saira, bun di":
+                                    st.balloons()
+
+if page == "Welcome":
+    st.title("Romansh for you")
+    st.markdown("""
+    **Romansh for you** is a :rainbow[completely free] way to learn Romansh for everyone.
+    
+    The way it works is that at the beginning of every lesson there is a button like this:""")
+    st.button("Start")
+    st.markdown("That reveals a table like this:")
+    df = pd.DataFrame({
+        'English': ["Hello", "Good morning", "Good afternoon", "How"],
+        'Romansh': ["Allegra", "Bun di", "Buna saira", "Co"]
+    })
+
+    df
+    st.markdown("This table shows your vocabulary for that lesson. Once you have memorised those words, you can start answering the questions. When you have finished all the questions, you will see balloons go across the screen")
+    if st.button("Show example balloons"):
+        st.balloons()
+    st.markdown("""The questions in this course are **case sensitive**, so if nothing happens when you enter your answer check you have mirrored the punctuation in the question.""")
+    
+    st.markdown("To enter you answer press **ctrl+enter**")
+    st.title("Chats")
+    st.markdown("""
+    In this course, there are multiple chats. When you enter a chat lesson, type in the given starting word **in Romansh**. The bot will write its response back, and what you should write next will appear under that. **If you do not write the correct response, the chat will not work!.** You repeat that until you are told the chat has finished. **You only write things in Romansh in a chat lesson**. """)
+
+answer = 1
+
+if page == "Chat 1":
+    answer = st.chat_input("")
+    st.title("Chat 1")
+    st.caption("You can start by saying 'hello'")
+
+    if answer == "Allegra":
+        with st.chat_message("assistant"):
+            st.markdown("Bun di")
+        st.write("Respond with 'How are you?'")
+    elif answer == "Co vai?":
+        with st.chat_message("Assistant"):
+            st.markdown("Bain grazia, e ti?")
+        st.write("Respond with 'Well'")
+    elif answer == "Bain":
+        with st.chat_message("assistant"):
+            st.markdown("Bun")
+        st.write("Respond with 'Goodbye'")
+    elif answer == "A revair":
+        with st.chat_message("assistant"):
+            st.markdown("A revair!")
+        st.success("You just completed your first chat!")
+        st.balloons()
+
+if page == "Lesson 2":
+    if st.button("Start"):
+        df = pd.DataFrame({
+            'English': ["How are you?", "Good", "Goodbye", "Well", "Thank you", "And you?"],
+            'Romansh': ["Co vai?", "Bun", "A revair", "Bain", "Grazia", "E ti?"]
+            })
+
+    df
+    st.markdown("---")
+    st.markdown("---")
+    
+    L2Q1 = st.text_area("What is 'How are you?'")
+    if L2Q1 == "Co vai?":
+        L2Q2 = st.text_area("What is 'Good afternoon, how are you? Well'")
+        if L2Q2 == "Buna saira, co vai? Bain":
+            L2Q3 = st.text_area("What is 'Good bye, thank you'")
+            if L2Q3 == "A revair, grazia":
+                L2Q4 = st.text_area("What is 'Good, and you?'")
+                if L2Q4 == "Bun, e ti?":
+                    L2Q5 = st.text_area("What is 'Thank you, how are you?'")
+                    if L2Q5 == "Grazia, co vai?":
+                        L2Q6 = st.text_area("What is 'Well, thank you. Goodbye'")
+                        if L2Q6 == "Bain, grazia. A revair":
+                                L2Q8 = st.text_area("What is 'Good morning, how are you?'")
+                                if L2Q8 == "Bun di, co vai?":
+                                    st.balloons()
+                    
