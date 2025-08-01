ef55687965134f3926c60c8485f5e7e049358cc3
Simjrn <simonjenni374@gmail.com>
2025-08-01 16:08:35 +0100

    Update RG_grammar.py

--- learn romansh/RG_grammar.py
+++ learn romansh/RG_grammar.py
@@ -1,46 +1,49 @@
 import streamlit as st
 
-def page_2():
-  st.set_page_config(page_title="Romansh grammar", page_icon="📚", layout="wide")
 
-  st.markdown("""
-**THIS AND THAT**
+st.set_page_config(page_title="Romansh grammar", page_icon="📚", layout="wide")
+def this_and_that():
+  st.markdown('''
+  **THIS AND THAT**
 
-In Romansh, the words "tschel" and "tschella" are **only** used as pronouns, and are never used before another noun. The ones that are used as adjectives/specifiers are: "quest", "quests", "questa" and "questas" which are introduced later on in the course.
+  In Romansh, the words "tschel" and "tschella" are **only** used as pronouns, and are never used before another noun. The ones that are used as adjectives/specifiers are: "quest", "quests", "questa" and "questas" which are introduced later on in the course.
+  ''')
 
-**FUTURE TENSE**
+def future():
+  st.markdown('''**FUTURE TENSE**
 
-To make a verb in the future tense, add:
+  To make a verb in the future tense, add:
 
-• Jau vegn
+  • Jau vegn
 
-• Ti vegns
+  • Ti vegns
 
-• El vegn
+  • El vegn
 
-• Nus vegnin
+  • Nus vegnin
 
-• Vus vegnis
+  • Vus vegnis
 
-• Els vegnan
+  • Els vegnan
 
-, then "a" (or "ad" before a vowel) and put the verb after, so "I will buy" is "Jau vegn a cumprar".
+  , then "a" (or "ad" before a vowel) and put the verb after, so "I will buy" is "Jau vegn a cumprar".''')
 
-**PRESENT TENSE**
+def present():
+  st.markdown('''**PRESENT TENSE**
 
-To form the present tense in Romansh, take away the final -ir, -ar, -air or -er and add these endings:
+  To form the present tense in Romansh, take away the final -ir, -ar, -air or -er and add these endings:
 
              
-          • Either keep the verb the same or sometimes add the endings "el" or "un" for the pronoun "jau"
-          • Add "-as" for the pronoun "ti"
-          • Add "-a" for the pronoun "el"
-          • Add "-ais" for the pronoun "vus"
-          • Add "-ain" for the pronoun "nus"
-          • Add "-an" for the pronoun "els"
+            • Either keep the verb the same or sometimes add the endings "el" or "un" for the pronoun "jau"
+            • Add "-as" for the pronoun "ti"
+            • Add "-a" for the pronoun "el"
+            • Add "-ais" for the pronoun "vus"
+            • Add "-ain" for the pronoun "nus"
+            • Add "-an" for the pronoun "els"
+            ''')
 
-
-
-**PAST TENSE**
+def past_tense():
+  st.markdown('''**PAST TENSE**
 
 To make a verb past tense, it's a bit more complicated. These are the rules:
 
@@ -68,28 +71,35 @@ To make a verb past tense, it's a bit more complicated. These are the rules:
                           • -ivan for the pronouns "nus" and "els"
 
                           • -ivas for the pronouns "ti" and "vus"
-Note that there are often exceptions to verb conjugation rules
+Note that there are often exceptions to verb conjugation rules''')
 
-**NEGATION**
+def negation():
+  st.markdown('''**NEGATION**
 
 To negate a word in romansh, you put "na" (or n'  before a vowel) before the verb and "betg" after the verb eg. "jau n'aveva betg" (I didn't have) or "el na po betg" (He can't) 
+''')
 
-**IMPERATIVE**
+def imperative():
+  st.markdown('''**IMPERATIVE**
 
-To make an order in romansh, just take away the ending (-ar, -er or -ir) and replace it with '-a'
+To make an order in romansh, just take away the ending (-ar, -er or -ir) and replace it with '-a''')
 
-**PAST PARTICIPLE**
+def past_participle():             
+  st.markdown('''**PAST PARTICIPLE**
 
 To say that you have done something, you use 'to have' (or 'to be' if the verb ends in 'ir') (both conjugated correctly with the pronoun), and change the verb that you have done like this:
                • If the verb ends in '-ar', replace the '-ar' with '-à'
                • If the verb ends in '-er, -air or -ir', replace the '-er, -air or -ir' with '-ì'   
 For example 'Jau sun partì'(I have gone away) or 'Ti has  gidà'(You have helped)
+''')
 
-**GENDER**
+def gender():
+  st.markdown('''**GENDER**
 
 Just like Spanish or Italian, Romansh is a gendered language with 2 genders. Also like other languages, a good way to distinguish masculine from femenine in most words (some don't end in either) is if it ends in an 'A' it's feminine, and if it ends in an 'O' it's masculine. Depending on the gender, you will have to use different articles and adjectives, not only including objects but also yourself, so if you were a woman, you'd say "jau sun pitschna" but if you were a man, you'd say "Jau sun pitschen".
-
-**NUMBERS**
+''')
+def numbers():  
+st.markdown('''**NUMBERS**
 
 **21-99:**
 
@@ -102,7 +112,7 @@ Counting from 100 to 999 is very simple: just use this formula:
 **Amount of hundreds + e (Which means "and", and changes to "ed" before vowels) + the number from 1-99.**
 
 It is important to remember that e changes before vowels
-  """)
+''')
   
-pg = st.navigation(["Trying_out_python_code.py", page_2])
+pg = st.navigation([this_and_that, future, present, past_tense, negation, imperative, past_participle, gender, nmbers])
 pg.run()
