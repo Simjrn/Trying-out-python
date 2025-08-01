f76c4fb324092c67d9b9be11785b7d585fe0b052
Simjrn <simonjenni374@gmail.com>
2025-08-01 12:16:07 +0100

    Update RG_grammar.py

--- learn romansh/RG_grammar.py
+++ learn romansh/RG_grammar.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.page_config(page_title="Romansh grammar", page_icon="🇨🇭", layout="wide")
+set.page_config(page_title="Romansh grammar", page_icon="🇨🇭", layout="wide")
 
 st.markdown("""
 **THIS AND THAT**
