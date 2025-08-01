4ee96c0133d6d016a139a64b2c350ac7f57b060a
Simjrn <simonjenni374@gmail.com>
2025-08-01 11:39:17 +0100

    Create RG_grammar.py

---
+++ RG_grammar.py
@@ -0,0 +1,104 @@
+import streamlit as st
+
+st.page_config(page_title="Romansh grammar", page_icon="🇨🇭", layout="wide")
+
+st.markdown("""
+**THIS AND THAT**
+
+In Romansh, the words "tschel" and "tschella" are **only** used as pronouns, and are never used before another noun. The ones that are used as adjectives/specifiers are: "quest", "quests", "questa" and "questas" which are introduced later on in the course.
+
+**FUTURE TENSE**
+
+To make a verb in the future tense, add:
+
+• Jau vegn
+
+• Ti vegns
+
+• El vegn
+
+• Nus vegnin
+
+• Vus vegnis
+
+• Els vegnan
+
+, then "a" (or "ad" before a vowel) and put the verb after, so "I will buy" is "Jau vegn a cumprar".
+
+**PRESENT TENSE**
+
+To form the present tense in Romansh, take away the final -ir, -ar, -air or -er and add these endings:
+
+             
+          • Either keep the verb the same or sometimes add the endings "el" or "un" for the pronoun "jau"
+          • Add "-as" for the pronoun "ti"
+          • Add "-a" for the pronoun "el"
+          • Add "-ais" for the pronoun "vus"
+          • Add "-ain" for the pronoun "nus"
+          • Add "-an" for the pronoun "els"
+
+
+
+**PAST TENSE**
+
+To make a verb past tense, it's a bit more complicated. These are the rules:
+
+•If the verb endes in "ar", replace the "ar" with:
+
+                          
+                          • -ava for the pronouns "jau" and "el"
+                          
+                          • -avan for the pronouns "nus" and "els"
+                          
+                          • -avas for the pronouns "ti" and "vus"
+•If the verb ends in "er" or "air", replace the "air" or "er":
+
+                          
+                          • -eva for the pronouns "jau" and "el"
+                          
+                          • -evan for the pronouns "nus" and "els"
+                          
+                          • -evas for the pronouns "ti" and "vus"
+•If the verb ends in "ir", replace the "ir" with:
+
+      
+                          • -iva for the pronouns "jau" and "el"
+
+                          • -ivan for the pronouns "nus" and "els"
+
+                          • -ivas for the pronouns "ti" and "vus"
+Note that there are often exceptions to verb conjugation rules
+
+**NEGATION**
+
+To negate a word in romansh, you put "na" (or n'  before a vowel) before the verb and "betg" after the verb eg. "jau n'aveva betg" (I didn't have) or "el na po betg" (He can't) 
+
+**IMPERATIVE**
+
+To make an order in romansh, just take away the ending (-ar, -er or -ir) and replace it with '-a'
+
+**PAST PARTICIPLE**
+
+To say that you have done something, you use 'to have' (or 'to be' if the verb ends in 'ir') (both conjugated correctly with the pronoun), and change the verb that you have done like this:
+               • If the verb ends in '-ar', replace the '-ar' with '-à'
+               • If the verb ends in '-er, -air or -ir', replace the '-er, -air or -ir' with '-ì'   
+For example 'Jau sun partì'(I have gone away) or 'Ti has  gidà'(You have helped)
+
+**GENDER**
+
+Just like Spanish or Italian, Romansh is a gendered language with 2 genders. Also like other languages, a good way to distinguish masculine from femenine in most words (some don't end in either) is if it ends in an 'A' it's feminine, and if it ends in an 'O' it's masculine. Depending on the gender, you will have to use different articles and adjectives, not only including objects but also yourself, so if you were a woman, you'd say "jau sun pitschna" but if you were a man, you'd say "Jau sun pitschen".
+
+**NUMBERS**
+
+**21-99:**
+
+To count from 21-99, you just add the word for the second digit onto the end of the word for the first digit, as in "trentadus", with the exceptions of 22-27 and 29, where you put "a" between the two digits and any number greater than 30 that ends in 1 or 8, where you exclude the vowel from the end of the word meaning the first digit, as in "novantin" not "novantain".
+
+**100-999:**
+
+Counting from 100 to 999 is very simple: just use this formula:
+
+**Amount of hundreds + e (Which means "and", and changes to "ed" before vowels) + the number from 1-99.**
+
+It is important to remember that e changes before vowels
+""")
