1de50ec65e16414a30cd493ea59209e2e322f167
Simjrn <simonjenni374@gmail.com>
2025-08-01 11:00:25 +0100

    Update Trying_out_python_code.py

--- Trying_out_python_code.py
+++ Trying_out_python_code.py
@@ -144,9 +144,11 @@ if page == "Chat 1":
     answer = st.chat_input("")
     st.title("Chat 1")
     st.caption("You can start by saying 'hello'")
-    if progress_on == 0:
-        placeholder = st.empty()
-        placeholder.progress(0)
+    while True:
+        if progress_on == 0:
+            placeholder = st.empty()
+            placeholder.progress(0)
+            st.rerun()
 
     if answer == "Allegra":
         progress_on = 1
